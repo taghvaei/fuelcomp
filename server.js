f5c8880b62eca236d38486c5820fde2797c16071
Ruslan Kyba <kybargr@gmail.com>
2017-04-14 14:06:00 +0300

    Kepp app alive

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
 require('babel-core/register');
 require('./src/server');
+
+// Keep app alive
+var http = require('http');
+setInterval(function() {
+    http.get('https://fuelcomp.herokuapp.com/');
+}, 300000); // every 5 minutes (300000)

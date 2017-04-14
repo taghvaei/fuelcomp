bb686a3dfe439953c4f5d3a491d67623e98239c3
Ruslan Kyba <kybargr@gmail.com>
2017-04-15 02:03:03 +0300

    Disabled fake self calls

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 require('babel-core/register');
 require('./src/server');
 
-// Keep app alive
-var https = require('https');
-setInterval(function() {
-    https.get('https://fuelcomp.herokuapp.com/');
-}, 300000); // every 5 minutes (300000)
+// // Keep app alive
+// var https = require('https');
+// setInterval(function() {
+//     https.get('https://fuelcomp.herokuapp.com/');
+// }, 300000); // every 5 minutes (300000)

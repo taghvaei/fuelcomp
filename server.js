71aa62d06e0cb0f95040069d4793b49749f4ccb3
Ruslan Kyba <kybargr@gmail.com>
2017-04-14 14:13:17 +0300

    Switching to https

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('babel-core/register');
 require('./src/server');
 
 // Keep app alive
-var http = require('http');
+var https = require('https');
 setInterval(function() {
-    http.get('https://fuelcomp.herokuapp.com/');
+    https.get('https://fuelcomp.herokuapp.com/');
 }, 300000); // every 5 minutes (300000)
